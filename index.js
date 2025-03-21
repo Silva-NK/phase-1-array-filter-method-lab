fddf0566af103a20b0c062dca550cb72250871b2
Silva-NK <kiagonsuzanne@gmail.com>
2025-03-22 02:00:20 +0300

    Adds three filter functions to index.js.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution here
+// const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"]
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+];
+
+function matchName(drivers, searchName) {
+    return drivers.filter(driver => driver.name.toLowerCase() === searchName.toLowerCase());
+}
